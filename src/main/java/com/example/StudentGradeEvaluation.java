8d76c31137c6e7a889e649e4fe4e74229a5f5bb5
MekaManojKrishnalakshman <mekamanojkrishnalakshman@gmail.com>
2024-06-21 11:28:37 +0530

    Score

--- src/main/java/com/example/StudentGradeEvaluation.java
+++ src/main/java/com/example/StudentGradeEvaluation.java
@@ -13,9 +13,9 @@ public class StudentGradeEvaluation {
         int age=scanner.nextInt();
         System.out.println("Enter your exam score:");
         int score=scanner.nextInt();
-        System.out.println("Name:" +name);
-        System.out.println("Age:"  +age);
-        System.out.println("Score:"  +score);
+        System.out.println("Name :" + name);
+        System.out.println("Age :"  + age);
+        System.out.println("Score :"  + score);
 
 
 
